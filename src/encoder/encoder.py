e20fd91c71013631a8fb7a3fbd674fc0af2b9d89
Christian Fleury <chfleurysiq@gmail.com>
2023-06-15 13:46:26 -0300

    refactor: make code more clear

--- src/encoder/encoder.py
+++ src/encoder/encoder.py
@@ -18,61 +18,49 @@ GPIO.setup(CLK_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # CLK_GPIO
 def handleEncoder():
     delta = 0
     CLK_reference = bool(GPIO.input(CLK_GPIO))  
-    estadoBotao = GPIO.HIGH  # Estado atual do botão
-    ultimoEstadoBotao = GPIO.HIGH  # Estado anterior do botão
-    contadorCliques = 0  # Contador de cliques
-    tempoLimite = 0.25  # Tempo limite entre os cliques duplos (em segundos)
-    tempoUltimoClique = 0  # Tempo em que o último clique ocorreu
+    buttonState = GPIO.HIGH
+    lastButtonState = GPIO.HIGH
+    clickCount = 0
+    timeLimitToDoubleClick = 0.25
+    lastClickTime = 0
 
     while True:
         CLK = bool(GPIO.input(CLK_GPIO))
         DT = bool(GPIO.input(DT_GPIO))
-        estadoBotao =  GPIO.input(SW_GPIO)
+        buttonState =  GPIO.input(SW_GPIO)
 
-        # Verificar se houve uma mudança de estado
-        if estadoBotao != ultimoEstadoBotao:
+        if buttonState != lastButtonState:
             time.sleep(0.01)
             
-            # Verificar se o botão foi pressionado (passou de HIGH para LOW)
-            if estadoBotao == GPIO.LOW:
-                # Verificar se ocorreu um clique duplo dentro do tempo limite
-                if (time.time() - tempoUltimoClique) < tempoLimite:
-                    contadorCliques = 2  # Clique duplo detectado
+            if buttonState == GPIO.LOW:
+                if (time.time() - lastClickTime) < timeLimitToDoubleClick:
+                    clickCount = 2
                 else:
-                    contadorCliques += 1  # Contabilizar clique único
-                tempoUltimoClique = time.time()  # Atualizar o tempo do último clique
+                    clickCount += 1
+                lastClickTime = time.time()
 
-        # Atualizar o estado anterior do botão
-        ultimoEstadoBotao = estadoBotao
+        lastButtonState = buttonState
 
-        # Realizar ações com base no número de cliques detectados
-        if contadorCliques == 1 and (time.time() - tempoUltimoClique) > tempoLimite:
-            # Ação para clique único
+        if clickCount == 1 and (time.time() - lastClickTime) > timeLimitToDoubleClick:
             print("Clique único detectado")
-            contadorCliques = 0
+            clickCount = 0
 
 
-        elif contadorCliques == 2:
-            # Ação para clique duplo
+        elif clickCount == 2:
             print("Clique duplo detectado")
 
-        # Reiniciar o contador de cliques após realizar as ações
-        if contadorCliques >= 2:
-            contadorCliques = 0
-
-        # Pequeno atraso para evitar detecção falsa de cliques
+        if clickCount >= 2:
+            clickCount = 0
 
         if CLK != CLK_reference:
             if DT != CLK:
                 delta += 1
             else:
                 delta -= 1
-            # while bool(GPIO.input(CLK_GPIO)):
-            #     pass
                 
             print(delta)
            
-            # time.sleep(0.003)
+        time.sleep(0.0003)
                 
         CLK_reference = CLK
         
