cc60efac38f145b919ea3b4f9d19541da4c0d9fe
Christian Fleury <chfleurysiq@gmail.com>
2023-06-16 22:25:30 -0300

    checkpoint

--- src/modbus/modbus.py
+++ src/modbus/modbus.py
@@ -4,7 +4,9 @@ import struct
 
 class ModBus:
     def __init__(self):
-        self.uart = uart.init_UART()
+        self.myAddress = 0x00
+        self.uart = uart.Uart()
+        self.uart.init_UART()
 
     def write(self, target_address, code, subcode, matricula, data):
         try:
@@ -36,95 +38,21 @@ class ModBus:
 
             for i in tx_buffer:
                 print(i)
-            uart.write_UART(self.uart, tx_buffer)
+            self.uart.write_UART(tx_buffer)
         except Exception as e:
             print(e)
-        #    printf("Digite o comando: ");
-        # scanf("%x", &command);
 
-        # unsigned int matricula[TAMANHO_MATRICULA] = {1, 6, 0, 2};
-        # unsigned char tx_buffer[300];
-        # unsigned char *p_tx_buffer;
 
-        # p_tx_buffer = &tx_buffer[0];
+    def read(self):
+        data_buffer = self.uart.read_UART()
+        index = 0
+        if data_buffer != 0 and data_buffer != 1:
+            if data_buffer[index] != self.myAddress:
+                return -1
+            index += 1
 
-        # // ADDRESS FIELD
-        # *p_tx_buffer++ = target_device_address;
-        # // CODE FIELD
-        # *p_tx_buffer++ = mod_bus_code;
 
-        # // DATA FIELD
-        # memcpy(p_tx_buffer++, &command, 1);
-
-        # if (command == 0xB1)
-        # {
-        #     int data;
-        #     printf("Digite o inteiro a ser enviado.\n");
-
-        #     scanf("%d", &data);
-
-        #     memcpy(p_tx_buffer, &data, sizeof(data));
-        #     p_tx_buffer += sizeof(data);
-        # }
-        # else if (command == 0xB2)
-        # {
-        #     float data;
-        #     printf("Digite o float a ser enviado.\n");
-
-        #     scanf("%f", &data);
-
-        #     memcpy(p_tx_buffer, &data, sizeof(data));
-        #     p_tx_buffer += sizeof(data);
-        # }
-        # else if (command == 0xB3)
-        # {
-        #     printf("Digite o tamanho da string a ser enviada.\n");
-        #     int size;
-        #     scanf("%d", &size);
-
-        #     printf("Digite a string a ser enviada.\n");
-        #     char data[256];
-
-        #     scanf("%s", data);
-
-        #     memcpy(p_tx_buffer++, &size, 1);
-        #     memcpy(p_tx_buffer, &data[0], size);
-        #     p_tx_buffer += size;
-        # }
-
-        # // for (int i = 0; i < TAMANHO_MATRICULA; i++)
-        # // {
-        # //     memcpy(p_tx_buffer++, &matricula[i], 1);
-        # // }
-
-        # // TODO
-        # memcpy(p_tx_buffer++, &matricula[0], 1);
-        # memcpy(p_tx_buffer++, &matricula[1], 1);
-        # memcpy(p_tx_buffer++, &matricula[2], 1);
-        # memcpy(p_tx_buffer++, &matricula[3], 1);
-
-        # // CRC FIELD
-        # int data_field_size = (p_tx_buffer - &tx_buffer[0]) - 2;
-        # short CRC_16 = calcula_CRC(&tx_buffer[2], data_field_size);
-        # memcpy(p_tx_buffer, &CRC_16, sizeof(CRC_16));
-
-        # p_tx_buffer += sizeof(CRC_16);
-
-        # printf("Buffers de memória criados!\n");
-
-        # printf("Escrevendo caracteres na UART ...");
-
-        # int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));
-        # if (count < 0)
-        # {
-        #     printf("UART TX error\n");
-        # }
-        # else
-        # {
-        #     printf("escrito.%d\n", count);
-        # }
-        # sleep(1);
-
+        
 x = ModBus()
 
 x.write(0x01, 0x16, 0xD8 , (1, 6 ,0 , 2), "jairo")
--- src/modbus/uart/uart.py
+++ src/modbus/uart/uart.py
@@ -1,35 +1,49 @@
 import serial
 
-def init_UART():
-    print('chegou aqui')
-    try:
-        port = '/dev/serial0'  # Replace with the actual port name (e.g., '/dev/ttyUSB0' on Linux)
-        baud_rate = 9600  # Set the baud rate
-        timeout = 0
-        uart0_filestream = serial.Serial(port, baud_rate, timeout=timeout)
-        if uart0_filestream.is_open:
-            print("Serial port is already open.")
+class Uart:
+    def __init__(self):
+        self.uart0_filestream = 0
+
+    def init_UART(self):
+        print('chegou aqui')
+        try:
+            port = '/dev/serial0'  # Replace with the actual port name (e.g., '/dev/ttyUSB0' on Linux)
+            baud_rate = 9600  # Set the baud rate
+            timeout = 0
+            self.uart0_filestream = serial.Serial(port, baud_rate, timeout=timeout)
+            if self.uart0_filestream.is_open:
+                print("Serial port is already open.")
+            else:
+                # Open the serial port
+                self.uart0_filestream.open()
+                print("Serial port opened successfully.")
+
+            return self.uart0_filestream
+        except serial.SerialException as e:
+
+            print(f"Failed to open the serial port: {str(e)}")
+
+    def write_UART(self, buffer):
+        if self.uart0_filestream:
+            print('wrote')
+            self.uart0_filestream.write(buffer)
         else:
-            # Open the serial port
-            uart0_filestream.open()
-            print("Serial port opened successfully.")
+            print('failed to write')
+    
+    def read_UART(self):
+        if self.uart0_filestream:
+            data_buffer = bytearray()
 
-        return uart0_filestream
-    except serial.SerialException as e:
+            data = self.uart0_filestream.read()
+            if data:
+                data_buffer.extend(data)
+                return data_buffer
+            else:
+                print('no content')
+                return 0
+        else:
+            print('failed to read')
+            return -1
 
-        print(f"Failed to open the serial port: {str(e)}")
-
-def write_UART(uart0_filestream, buffer):
-    if uart0_filestream:
-        print('wrote')
-        uart0_filestream.write(buffer)
-    else:
-        print('failed to write')
- 
-def read_UART(uart0_filestream):
-    if uart0_filestream:
-        data = uart0_filestream.read()
-        return data
-    else:
-        print('failed to read')
-        return -1
+    def close_UART(self):
+        self.uart0_filestream.close()
